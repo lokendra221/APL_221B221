92bd8a47a68bc1492bff7b96d60abca1bc2464ea
221B221 <221B221@juet.co.in>
2025-03-28 14:14:41 +0530

    lab 5

---
+++ LabE05/Prob01/Beverage.java
@@ -0,0 +1,17 @@
+abstract class Beverage{
+  private void pour(int qty){
+      System.out.println("Add"+ qty+ "ml of beverage");
+  }
+  
+   protected abstract void addCondiment();
+   protected void stir(){   }
+   private void serve(){
+       System.out.println("serve through waiter");
+   }
+      public  void templateMethod (int qty){
+        pour(qty);
+        addCondiment();
+        stir();
+        serve();
+    }
+}
---
+++ LabE05/Prob01/Whiskey.java
@@ -0,0 +1,12 @@
+class Whiskey extends Beverage {
+  
+    protected void addCondiment(){
+        System.out.println("Add Ice");
+        
+    }
+    protected void stir(){
+        System.out.println("Stire for 27 sec");
+    }
+ 
+  
+}
